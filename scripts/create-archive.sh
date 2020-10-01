9c627dfa279ca9629009131ae8e2b97c4406e932
Matt Dias <matt.dias6@gmail.com>
2020-09-30 21:05:31 -0400

    creating an archive

---
+++ scripts/create-archive.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eo pipefail
+xcodebuild -project ScoreKeeper.xcodeproj \
+           -scheme "ScoreKeeper" \
+           -sdk iphoneos \
+           -archivePath ./ScoreKeeper.xcarchive \
+           -configuration "ScoreKeeper" \
+           clean archive
---
+++ scripts/export-archive.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+set -eo pipefail
+xcodebuild -archivePath ./ScoreKeeper.xcarchive \
+           -exportOptionsPlist ./ExportOptions.plist \
+           -exportPath ./ScoreKeeperProducts \
+           -allowProvisioningUpdates \
+           -exportArchive
